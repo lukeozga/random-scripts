d414bc22b7ebd80c1fe0a1e203f3b9afe50fb7f8
Lukasz Ozga <lukaszozga1324@gmail.com>
2023-02-02 17:23:27 +0000

    Fix bugs in caesarCipher.py

--- python/caesarCipher.py
+++ python/caesarCipher.py
@@ -7,28 +7,31 @@ class CaesarCipher:
     def encode(self, text: str, shift: int, mode: str) -> str:
         if mode != "encrypt" and mode != "decrypt":
             sys.exit("Valid values of mode argument are: 'encrypt', 'decrypt'")
+        # Handle 'shift' parameter values > than maximum index 
+        new_shift = shift % (len(self.char_list) - 1) if shift > len(self.char_list) -1 else shift
         if isinstance(text, str):
             encoded_text = ""
             for letter in text:
-                isUpper = letter.isupper()
                 for index, char in enumerate(self.char_list):
                     if letter.lower() == char and mode == "encrypt":
-                        newIndex = index + shift if index + shift <= len(char_list) - 1 else index + shift - len(char_list) - 1
-                        encoded_text += self.char_list[newIndex] if not isUpper else str(self.char_list[newIndex]).upper()
+                        newIndex = index + new_shift if index + new_shift < len(self.char_list) - 1 else index + new_shift - len(char_list)
+                        encoded_text += str(self.char_list[newIndex]).capitalize()
                         break
                     elif letter.lower() == char and mode == "decrypt":
-                        newIndex = index - shift
-                        encoded_text += self.char_list[newIndex] if not isUpper else str(self.char_list[newIndex]).upper()
+                        newIndex = index - new_shift
+                        encoded_text += str(self.char_list[newIndex]).capitalize()
                         break
-                    elif index == len(char_list) - 1:
+                    elif index == len(self.char_list) - 1:
                         print(f"Character {letter} not known.")
             return encoded_text
         else:
             sys.exit("Funtion argument 'text' must be of value string")
+                
+
 
 char_list = [
     "a", "b" ,"c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
     "o", "p", "q", "r", "s", "t" ,"u", "v", "w", "x", "y", "z", "1", "2", 
-    "3", "4", "5", "6", "7", "8", "9", " ", "'", "\"" ".", ",", "!", "~", 
-    "@", "#", "$", "%", "(", ")", "~",
+    "3", "4", "5", "6", "7", "8", "9", " ", "'", "\"", ".", ",", "!", "~", 
+    "@", "#", "$", "%", "(", ")",
 ]
